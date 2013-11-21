56916a3609a155bd56ce38d32cc3fac5a25393ab
Karl Entwistle <karl@entwistle.com>
2013-11-21 15:10:06 +0000

    Fix sprite path generation in Rails 4.
    * get correct urls.

--- lib/compass-rails/patches/4_0.rb
+++ lib/compass-rails/patches/4_0.rb
@@ -2,14 +2,18 @@ require 'compass-rails/patches/sass_importer'
 require 'compass-rails/patches/sprite_importer'
 module Sass::Script::Functions
   def generated_image_url(path, only_path = nil)
-    asset_url(path)
+    asset_url(
+      Sass::Script::String.new(
+        File.join(Compass.configuration.default_http_images_path, path.to_s)
+      )
+    )
   end
 end
 
 
 module Compass::RailsImageFunctionPatch
   private
-  
+
   def image_path_for_size(image_file)
     begin
       file = ::Rails.application.assets.find_asset(image_file)
